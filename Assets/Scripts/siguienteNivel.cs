f7841ba6d03544e769771093f5ffe8b1f41f5af4
SilesR <rafasiles@gmail.com>
2014-10-15 16:43:33 +0200

    Inicio

---
+++ Assets/Scripts/siguienteNivel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siguienteNivel : MonoBehaviour {
+
+	public string nivel = "Pollo2";
+
+	// Use this for initialization
+		void OnTriggerEnter2D(Collider2D other){
+		Siguiente ();
+		}
+	public void Siguiente(){
+		Application.LoadLevel (nivel);
+	}
+}
