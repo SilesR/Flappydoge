1fca872e449e2bf4fbbd166b2fb214228e044395
SilesR <rafasiles@gmail.com>
2014-10-15 18:08:16 +0200

    Scroll Horizontal

--- Assets/Scripts/bombaScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class bombaScript : MonoBehaviour {
-	void OnTriggerEnter2D(Collider2D other){
-		Debug.Log ("Se ha chocado contra la bomba");
-		}
-}
