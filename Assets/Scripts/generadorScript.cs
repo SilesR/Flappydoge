d33931f3c5946c61a4ea054261b037a3fdcfe177
SilesR <rafasiles@gmail.com>
2014-10-20 18:50:43 +0200

    Diferentes tipos de columnas (listas)

--- Assets/Scripts/generadorScript.cs
+++ Assets/Scripts/generadorScript.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class generadorScript : MonoBehaviour {
-	public GameObject columna;
+	public GameObject[] columna;
 	public float tiempoespera = 3;
 	Vector3 posicion;
 	float timer = 0;
@@ -14,8 +14,16 @@ public class generadorScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > timer) {
-			var nuevacolumna = (GameObject)Instantiate(columna, posicion, transform.rotation);
+
+			var altura = Random.Range(-1.5f,1.5f);
+			var tipocolumna = Random.Range(0,columna.Length);
+			var nuevacolumna = (GameObject)Instantiate(columna[tipocolumna],new Vector3 (posicion.x, posicion.y+altura, posicion.z),transform.rotation);
+
 			timer = Time.time+tiempoespera;
+
+			Destroy(nuevacolumna, tiempoespera*3);
+
+			timer=Time.time+tiempoespera;
 				}
 	}
 }
