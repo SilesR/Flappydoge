ba7acf1882820531b36a71d2eb8f390a7a0a8b5c
SilesR <rafasiles@gmail.com>
2014-10-20 16:43:24 +0200

    Movimiento Flechas

---
+++ Assets/Scripts/movimientoScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimientoScript : MonoBehaviour {
+
+	public int vhorizontal = 10;
+	public int vvertical = 10;
+
+	Vector3 movimiento;
+	// Update is called once per frame
+	void Update () {
+
+		var v = Input.GetAxis ("Vertical");
+		var h = Input.GetAxis ("Horizontal");
+		movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
+
+		transform.Translate (movimiento * Time.deltaTime);
+
+	
+	}
+}
