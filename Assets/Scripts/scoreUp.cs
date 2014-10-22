549ec89210e41d1097e02c6a6b6e7357f86e772c
SilesR <rafasiles@gmail.com>
2014-10-22 18:50:12 +0200

    Canvas_Marcador

--- Assets/Scripts/scoreUp.cs
+++ Assets/Scripts/scoreUp.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class scoreUp : MonoBehaviour {
+
+	public Text texto;
+	void Start(){
+				texto = GameObject.Find ("textonivel").GetComponent<Text> ();
+		}
 
 	void OnTriggerEnter2D(Collider2D col){
 				
 		GameControl.score = GameControl.score + 1;
-		Debug.Log (GameControl.score);
+
+		texto.text = "Level " + GameControl.score.ToString ();
+
 		}
 
 	// Update is called once per frame
