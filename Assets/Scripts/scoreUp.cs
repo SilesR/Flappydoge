08a3445f42ff4c13fc34001dca9402b9d6c5310c
SilesR <rafasiles@gmail.com>
2014-10-22 17:48:07 +0200

    Puntuacion!

---
+++ Assets/Scripts/scoreUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoreUp : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+				
+		GameControl.score = GameControl.score + 1;
+		Debug.Log (GameControl.score);
+		}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
